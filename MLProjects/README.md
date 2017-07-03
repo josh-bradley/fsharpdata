ba173516ec31ba23a153351212711d43c38311aa
Josh <joshjb84@hotmail.com>
2017-07-03 20:51:23 +1000

    more chapter 2

--- MLProjects/README.md
+++ MLProjects/README.md
@@ -1,3 +1,8 @@
-# Dataset from 
+# Chapter 1 
 
 [Kaggle comp](https://www.kaggle.com/c/digit-recognizer) save "train.csv" to /data and run the MakeDataSubset.fsx
+
+# Chapter 2
+
+[Stop words](http://xpo6.com/download-stop-word-list/)
+[SMS data](https://onedrive.live.com/?id=2BCBE7155876A728%217543&cid=2BCBE7155876A728)
--- MLProjects/chapter2/chapter2.fsx
+++ MLProjects/chapter2/chapter2.fsx
@@ -1,7 +1,8 @@
 #load @"NaiveBayes.fsx"
+#load @"getData.fsx"
 
 open NaiveBayes
-
+open GetData
 open System.IO
 
 type DocType = Ham | Spam
@@ -18,7 +19,7 @@ let parseLine (line:string) =
     (label, message)
 
 let fileName = "SMSSpamCollection"
-let path = __SOURCE_DIRECTORY__ + @"..\data\" + fileName
+let path = __SOURCE_DIRECTORY__ + @"/data/" + fileName
 
 let dataset = File.ReadLines path
                 |> List.ofSeq
@@ -29,7 +30,7 @@ open System.Text.RegularExpressions
 
 let matchWords = Regex(@"\w+")
 
-let tokens (text:string) =
+let wordTokenizer (text:string) =
     text.ToLowerInvariant()
     |> matchWords.Matches
     |> Seq.cast<Match>
@@ -37,19 +38,109 @@ let tokens (text:string) =
     |> Set.ofSeq
 
 let training = dataset
-                |> List.take 4574
+                |> List.skip 100 
                 |> Array.ofList
 
-let txtClassifier = train training tokens (["txt"] |> set)              
 
 let validation = dataset
-                  |> List.skip 4574
-                  |> Array.ofList
+                  |> List.take 1000
+                  |> Array.ofList            
+
+let vocabulary (tokenizer:Tokenizer) (corpus:string seq) = 
+    corpus
+    |> Seq.map tokenizer
+    |> Set.unionMany
+
+let allTokens =
+    training
+    |> Seq.map snd
+    |> vocabulary wordTokenizer
+
+let fullClassifier = train training wordTokenizer allTokens
+
+let txtClassifier = train training wordTokenizer (["txt"] |> set)              
 
 
-validation
-|> Seq.averageBy (fun (docType,sms) -> 
-                    match (txtClassifier sms) = docType with
-                    | true ->  1.0
-                    | false -> 0. )
-|> printfn "Based no 'txt', correctly classified:%.3f"                
+let evaluate (tokenizer:Tokenizer) (tokens:Token Set) = 
+    let classifier = train training tokenizer tokens
+
+    validation
+    |> Seq.averageBy (fun (docType,sms) -> 
+                        match (classifier sms) = docType with
+                        | true ->  1.0
+                        | false -> 0. )
+    |> printfn "Correctly classified:%.3f"                
+
+evaluate wordTokenizer allTokens
+
+
+let caseTokenizer (text:string) =
+    text
+    |> matchWords.Matches
+    |> Seq.cast<Match>
+    |> Seq.map (fun m -> m.Value)
+    |> Set.ofSeq
+
+let casedTokens =
+    training
+    |> Seq.map snd
+    |> vocabulary caseTokenizer
+
+evaluate caseTokenizer casedTokens
+
+let top n (tokenizer:Tokenizer) (docs:string []) =
+    let tokenized = docs |> Array.map tokenizer
+    let allTokens = tokenized |> Set.unionMany
+    allTokens 
+    |> Seq.sortBy (fun t -> - countIn tokenized t)
+    |> Seq.take n
+    |> Set.ofSeq
+
+let ham,spam = 
+    let rawHam,rawSpam = 
+        training
+        |> Array.partition (fun (lbl,_) -> lbl = Ham)
+    rawHam |> Array.map snd,
+    rawSpam |> Array.map snd
+
+let countUniqueTokens (docs: string seq) =
+    docs |> vocabulary caseTokenizer |> Set.count
+
+let getTopPercentageOfTokens (percentage:int) count (docs:string []) =
+    let n = (float count) * (float percentage) / 100.
+    top (int n) caseTokenizer docs
+let hamCount = ham |> countUniqueTokens
+let spamCount = spam |> countUniqueTokens
+
+let topHamTokens = ham |> getTopPercentageOfTokens 1 hamCount
+let topSpamTokens = spam |> getTopPercentageOfTokens 1 spamCount
+
+let topTokens = Set.union topHamTokens topSpamTokens
+
+evaluate caseTokenizer topTokens
+
+let specificTokens = Set.intersect topHamTokens topSpamTokens
+                        |> Set.difference topTokens
+                        //|> evaluate caseTokenizer                                                
+
+let phoneRegex = Regex(@"0[7-9]\d{9}")
+let txtRegex = Regex(@"\b\d{5}\b")
+let (|PhoneNo|TextNo|Other|) str = 
+    if phoneRegex.Match(str).Length > 0 then 
+        PhoneNo
+    else if txtRegex.Match(str).Length > 0 then
+        TextNo
+    else 
+        Other
+
+let mapSpecialTokens str =
+    match str with
+    | PhoneNo -> "__PHONE__"    
+    | TextNo -> "__TEXT__"    
+    | Other -> str    
+
+let smartTokenizer = caseTokenizer >> Set.map mapSpecialTokens
+
+let smartTokens = specificTokens |> Set.add "__PHONE__" |> Set.add "__TEXT__"
+
+evaluate smartTokenizer smartTokens
